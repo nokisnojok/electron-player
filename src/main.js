db8174bfd02dffa185c52c9195e60945223a5c56
wanglin <mag1c_shen_09@foxmail.com>
2017-08-16 21:23:07 +0800

    debug the titile
    changge  titile to  player

--- src/main.js
+++ src/main.js
@@ -12,20 +12,3 @@ ReactDOM.render(
 	<Body a={{a:1}}></Body>,
 	document.getElementById('body')
 )
-function f(e){
-	e.preventDefault()
-	//console.log(e)
-	return false
-}
-document.ondragover=function(e){
-	e.preventDefault()
-	//console.log(e)
-	return false
-}
-document.ondrop=function(e){
-	e.preventDefault()
-	console.log(e.dataTransfer)
-	console.log(e.dataTransfer.files[0])
-	var file=e.dataTransfer.files[0]
-	console.log(file)
-}
