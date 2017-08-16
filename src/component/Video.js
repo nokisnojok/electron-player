77607cfc04f63cd1575fd095ad780338f1f85e27
wanglin <mag1c_shen_09@foxmail.com>
2017-08-16 18:37:11 +0800

    video component is ok
    video component is ok

--- src/component/Video.js
+++ src/component/Video.js
@@ -4,14 +4,11 @@ const { remote } =require('electron');
 
 const BrowserWindow = remote.BrowserWindow
 module.exports=class extends Component {
-	static defaultProps = {
-		
-	};
 	constructor(props) {
 		super(props);
 		global.video=this
 		this.state={
-			vid:'player',
+			vid:'',
 			paused: true,
 			volume: 0.3,
 			muted: false,
@@ -21,8 +18,24 @@ module.exports=class extends Component {
 		}
 	}
 	componentDidMount(){
-		this.bindEle(document.getElementById(this.state.vid))
+		console.log(this.props)
+		// if(this.props.src!=''&&this.props){
+		// 	console.log(this.props.src)
+		// 	this.play(this.props.src)
+		// }
+		this.bindEle(document.getElementById('player'))
 	}
+	// componentWillReceiveProps(props){
+	// 	if(props.src!=''){
+	// 		this.play(props.src)
+	// 	}
+	// }
+	// shouldComponentUpdate(nextProps,nextState){
+	// 	console.log('should..')
+	// 	console.log(nextProps)
+	// 	console.log(nextState)
+	// 	return true;
+	// }
 	test(v,cb){
 		var vELe=document.getElementById('player')
 		if(typeof v == 'string'){
@@ -34,14 +47,9 @@ module.exports=class extends Component {
 			}
 			vELe.oncanplay=(e)=>{
 				cb&&cb(true)
-				// vELe.play().then(()=>{
-				// 	this.setState({paused:false})
-				// },function(){})
 			}
 		}else if(typeof v == 'object'&&video.constructor===HTMLVideoElement){
-			console.log('video element')
 			var self=this
-			//检查是否可以播放
 			if(!v.paused){
 				this.bindEle(v)
 			}else{
@@ -52,13 +60,6 @@ module.exports=class extends Component {
 					console.log('it cant play')
 				})
 			}
-			// if(!v.paused){
-			// 	this.bindEle(v)
-			// }else if(v.readyState>2){
-			// 	this.bindEle(v)
-			// }else{
-			// 	console.log('it cant play')
-			// }
 		}else if(typeof v == 'undefined'){
 			cb&&cb(false)
 		}else{
@@ -66,24 +67,14 @@ module.exports=class extends Component {
 		}
 	}
 	play(v){
-		var vELe=document.getElementById('player')
+		var vELe=document.getElementById(this.state.vid)
+		var ele=document.getElementById(this.state.vid)
 		if(typeof v == 'string'){
+			console.log('do this.paly')
 			vELe.src=v
-			vELe.onerror=function(e){
-				this.onerror=null
-				console.log('error')
-				vELe.src=''
-			}
-			vELe.oncanplay=(e)=>{
-				console.log('canplay')
-				vELe.play().then(()=>{
-					this.setState({paused:false})
-				},function(){})
-			}
 		}else if(typeof v == 'object'&&v.constructor.name=='HTMLVideoElement'){
 			console.log('video element')
 			var self=this
-			//检查是否可以播放
 			if(!v.paused){
 				this.bindEle(v)
 			}else{
@@ -94,15 +85,10 @@ module.exports=class extends Component {
 					console.log('it cant play')
 				})
 			}
-
-			// if(!v.paused){
-			// 	this.bindEle(v)
-			// }else if(v.readyState>2){
-			// 	this.bindEle(v)
-			// }else{
-			// 	console.log('it cant play')
-			// }
 		}else if(typeof v == 'undefined'){
+			document.getElementById(this.state.vid).play().then(()=>{
+				//this.setState({paused:false})
+			},()=>{})
 			console.log(vELe.readyState)
 		}else{
 			console.log('what is this')
@@ -110,18 +96,27 @@ module.exports=class extends Component {
 	}
 	pause(){
 		if(!this.state.paused){
-			this.setState({paused:true})
 			document.getElementById(this.state.vid).pause()
 		}
 	}
 	unbindEle(ele){
+		if(!ele.paused){
+			ele.pause()
+			this.setState({paused:true})
+		}
+		ele.ontimeupdate=ele.onloadstart=ele.onloadeddata=ele.onloadmetadata=ele.ondurationchange=ele.onprogress=ele.oncanplay=ele.onended=ele.onerror=ele.onpause=ele.onplay=null
 		ele.ontimeupdate=null
 		ele.onended=null
 		ele.onerror=null
+		ele.onpause=null
+		ele.onplay=null
+		ele.onvolumechange=null
 	}
 	bindEle(ele){
 		//解绑
-		this.unbindEle(document.getElementById(this.state.vid))
+		console.log('bindEle')
+		if(this.state.vid!='')
+			this.unbindEle(document.getElementById(this.state.vid))
 		//更改声音大小
 		ele.volume=this.state.volume;
 		//绑定id
@@ -136,28 +131,63 @@ module.exports=class extends Component {
 		}else{
 			this.setState({paused:true})
 		}
+		//绑定时间
+		this.setState({duration:isNaN(ele.duration)?0:ele.duration})
 		//绑定事件
 		self=this
+		ele.onpause=function(e){
+			console.log(e)
+			self.setState({paused:this.paused})
+		}
+		ele.onplay=function(e){
+			console.log(e)
+			self.setState({paused:this.paused})
+		}
 		ele.ontimeupdate=function(e){
+			//console.log(e)
+			if(isNaN(this.duration)){
+				return
+			}
+			self.setState({currentTime:this.currentTime})
 			self.setState({progress:this.currentTime/this.duration})
 		}
+		ele.ondurationchange=function(e){
+			console.log(e)
+			self.setState({currentTime:this.currentTime})
+			self.setState({duration:this.duration})
+			self.setState({progress:this.currentTime/this.duration})
+			self.setState({paused:this.paused})
+		}
 		ele.onended=function(e){
+			console.log(e)
 			self.setState({paused:true})
-			console.log(this.currentTime)
 		}
 		ele.onerror=function(e){
+			console.log(e)
 			self.setState({paused:true})
+			self.setState({currentTime:this.currentTime})
+			self.setState({duration:0})
+			self.setState({progress:0})
+			self.setState({paused:this.paused})
+		}
+		ele.onvolumechange=function(e){
+			console.log(e)
+			if(this.volume==0||this.muted){
+				self.setState({muted:true})
+			}else{
+				self.setState({muted:false})
+			}
+			self.setState({volume:this.volume})
 		}
 		//append node
 		document.getElementById('playerContent').appendChild(ele)
 	}
 
 	playHandle(){
-		if(this.state.paused){
-			console.log('todo this.play')
+		var ele=document.getElementById(this.state.vid)
+		if(ele.paused){
 			this.play()
 		}else{
-			console.log('todo this.pause')
 			this.pause()
 		}
 	}
@@ -165,27 +195,29 @@ module.exports=class extends Component {
 		var ele=document.getElementById(this.state.vid)
 		if(ele.muted){
 			ele.muted=false
-			this.setState({muted:false})
 		}else{
 			ele.muted=true
-			this.setState({muted:true})
 		}
 	}
 	volumeHandle(e){
 		var ele=document.getElementById(this.state.vid)
 		var volumeEle=document.getElementById('volume')
 		var width=volumeEle.offsetWidth
-		var left=e.clientX-240
-		console.log(`left:${left}`)
-		console.log(`width:${width}`)
-		if(left==0){
-
-		}else{
+		var left=e.clientX-volumeEle.offsetLeft
+		if(left!=0){
 			ele.muted=false
-			this.setState({muted:false})
 		}
 		ele.volume=left/width
-		this.setState({volume:left/width})
+	}
+
+	progressHandle(e){
+		var ele=document.getElementById(this.state.vid)
+		var progressEle=document.getElementById('progress')
+		var width=progressEle.offsetWidth
+		var left=e.clientX-progressEle.offsetLeft
+		if(ele.duration==0||isNaN(ele.duration))
+			return
+		ele.currentTime=left/width*ele.duration
 	}
 	formatTime(second){
 		var min=parseInt(second/60);
@@ -195,11 +227,11 @@ module.exports=class extends Component {
 	render() {
 		return (
 			<div id='playerContent'>
-				<video id='player' width="100%" src="">
+				<video ref='v' id='player' width="100%" src="">
 				</video>
 				<div id='playerControl'>
-					<div className='play-progress' id='progress'>
-						<div style={{width:'50%'}} id='progress-left'></div>
+					<div className='play-progress' id='progress' onClick={this.progressHandle.bind(this)}>
+						<div style={{width:this.state.progress*100+'%'}} id='progress-left'></div>
 					</div>
 					<button className='play-btn' onClick={this.playHandle.bind(this)}>
 						<svg height="100%" version="1.1" viewBox="0 0 30 30" width="100%">
@@ -214,7 +246,7 @@ module.exports=class extends Component {
 						<use className="ytp-svg-shadow" xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#ytp-svg-13"></use>
 						<use className="ytp-svg-shadow" xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#ytp-svg-14"></use>
 						<path className="ytp-svg-fill" clipPath="url(#ytp-svg-volume-animation-mask)" d="M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 ZM19,11.29 C21.89,12.15 24,14.83 24,18 C24,21.17 21.89,23.85 19,24.71 L19,26.77 C23.01,25.86 26,22.28 26,18 C26,13.72 23.01,10.14 19,9.23 L19,11.29 Z" fill="#fff" id="ytp-svg-13"></path>
-						<path className="ytp-svg-fill"  d={!(this.state.muted||this.state.volume==0)?"M 9.25,9 7.98,10 7.98,10 l 0,0 Z":"M 9.25,9 7.98,10.27 24.71,27 l 1.27,-1.27 Z"} fill="#8CEA00" id="ytp-svg-14"></path>
+						<path className="ytp-svg-fill" style={{opacity:(this.state.muted||this.state.volume==0)?'1':'0'}} d={!(this.state.muted||this.state.volume==0)?"M 9.25,9 7.98,10 7.98,10 l 0,0 Z":"M 9.25,9 7.98,10.27 24.71,27 l 1.27,-1.27 Z"} fill="#8CEA00" id="ytp-svg-14"></path>
 						</svg>
 					</button>
 					<div className='play-volume' id="volume" onClick={this.volumeHandle.bind(this)}>
